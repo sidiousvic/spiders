b39dde6c6823cd5896d602e22552fba527c1e09f
sidiousvic <sidiousvic@gmail.com>
2020-09-27 20:55:54 +0900

    mv index.js > ts

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom";
+import React from "react";
+// import "./styles/styles.global.css";
+
+function App() {
+  return (
+    <>
+      <h1>Spiders 🕸</h1>
+      <h2>A web engineering log.</h2>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
