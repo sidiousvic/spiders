b680c3f66aa5742f308d309e2d20738245adcde7
sidiousvic <sidiousvic@gmail.com>
2020-09-27 21:54:01 +0900

    upd index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
-// import "./styles/styles.global.css";
+import Spiders from "./components/Spiders/Spiders.jsx";
 
-function App() {
-  return (
-    <>
-      <h1>Spiders 🕸</h1>
-      <h2>A web engineering log.</h2>
-    </>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Spiders />, document.getElementById("root"));
