3765323160dd2cc7cf6feb857827a64456264c55
sidiousvic <sidiousvic@gmail.com>
2020-09-27 21:53:54 +0900

    add Spiders.jsx

---
+++ src/components/Spiders/Spiders.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./Login/Login.jsx";
+
+export default function Spiders() {
+  return (
+    <>
+      <h1>Spiders 🕸</h1>
+      <h2>A web engineering log.</h2>
+      <Login />
+    </>
+  );
+}
