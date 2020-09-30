e198c59b12a784eb02f62bc6734271ef08f6678d
sidiousvic <sidiousvic@gmail.com>
2020-09-30 19:55:54 +0900

    using react router

--- src/components/Spiders/Spiders.tsx
+++ src/components/Spiders/Spiders.tsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Login/Login";
 
 export default function Spiders() {
-  const [token, setToken] = useState("");
-
   return (
-    <div id="spiders">
-      <h1>Spiders 🕸</h1>
-      <h2>A web engineering log.</h2>
+    <Router>
+      <li>
+        <Link to="/">home</Link>
+      </li>
+      <li>
+        <Link to="/weave">weave</Link>
+      </li>
+      <Switch>
+        <Route path="/weave">
+          <Weave />
+        </Route>
+        <Route path="/">
+          <div id="spiders">
+            <h1>Spiders 🕸</h1>
+            <h2>A web engineering log.</h2>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Weave() {
+  const [token, setToken] = useState("");
+  return (
+    <div id="weave">
       {!token && <Login setToken={setToken} />}
       {token && "Signed in"}
     </div>
