a6f7deafe4178874e7f801c77aad4f209ecba077
sidiousvic <sidiousvic@gmail.com>
2020-09-30 20:33:02 +0900

    add base href

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const { BaseHrefWebpackPlugin } = require("base-href-webpack-plugin"); // Or
 
 module.exports = {
   entry: {
@@ -30,6 +31,9 @@ module.exports = {
       template: "./public/index.html",
       filename: "index.html",
       favicon: "./public/favicon.ico",
+    }),
+    new BaseHrefWebpackPlugin({
+      baseHref: process.env.NODE_ENV === "dev" ? "/" : "/spiders",
     }),
   ],
   module: {
