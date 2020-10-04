c1503dd8b8bdb98f19beda7f2880932a5e8ffaf6
sidiousvic <sidiousvic@gmail.com>
2020-10-04 11:34:47 +0900

    provide graphql layer

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,11 +1,14 @@
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
 import auth from "./auth";
+import context from "./context";
+import { GraphqlLayer } from "src/types";
 
-const graphqlLayer = {
+const graphqlLayer: GraphqlLayer = {
   typeDefs,
   resolvers,
   auth,
+  context,
 };
 
 export default graphqlLayer;
