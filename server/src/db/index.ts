0460417c4300258afafce17b2b32b40d8b132b72
sidiousvic <sidiousvic@gmail.com>
2020-10-08 21:32:45 +0900

    created SpidersDatabase class/model

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,4 +1,3 @@
-import { DatabaseUtilsMap, DatabaseLayer, ModelsMap } from "../types";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,9 +8,36 @@ const PG_DATABASE = process.env.PG_DATABASE;
 const PG_HOST = process.env.PG_HOST;
 const PG_PORT = Number(process.env.PG_PORT);
 
-const DatabaseUtils: DatabaseUtilsMap = {
-  async runMigrations(pgm) {
-    console.log("🔧 Running migrations...");
+class SpidersDatabase {
+  pool?: Pool;
+
+  public async findUser(login: any) {
+    const {
+      rows: [user],
+    } = await this.pool!.query(
+      `SELECT * FROM users WHERE username = '${login.username}'`
+    );
+    if (user) return user;
+  }
+
+  public async addPost(post: any) {
+    await this.pool!.query(
+      `INSERT INTO posts (title, author, tags, body, updated_at, user_id, published, published_at) 
+      VALUES (
+          '${post.title}',
+          '${post.author}',
+          '${post.tags}',
+          '${post.body}',
+          to_timestamp(${Number(post.updatedAt) / 1000}),
+          '${post.userId}',
+          '${post.published}',
+          to_timestamp(${Number(post.publishedAt) / 1000})
+        )`
+    );
+  }
+
+  private async migrate() {
+    console.log("🐪 Running migrations...");
     await pgm({
       databaseUrl: `postgres://${PG_USER}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`,
       migrationsTable: "spiders_migrations",
@@ -19,59 +45,84 @@ const DatabaseUtils: DatabaseUtilsMap = {
       direction: "up",
       count: 9999,
     });
-  },
-  async buildModels(pool) {
-    console.log("🔨 Building database models...");
+  }
 
-    const models: ModelsMap = {
-      users: {
-        async findUser(login) {
-          const {
-            rows: [user],
-          } = await pool.query(
-            `SELECT * FROM users WHERE username = '${login.username}'`
-          );
-          if (user) return user;
-        },
-      },
-      posts: {
-        async addPost(post) {
-          await pool.query(
-            `INSERT INTO posts (title, author, tags, body, updated_at, user_id, published, published_at) 
-            VALUES (
-                '${post.title}',
-                '${post.author}',
-                '${post.tags}',
-                '${post.body}',
-                to_timestamp(${Number(post.updatedAt) / 1000}),
-                '${post.userId}',
-                '${post.published}',
-                to_timestamp(${Number(post.publishedAt) / 1000})
-              )`
-          );
-        },
-      },
-    };
-    return models;
-  },
-  async connectDatabase(runMigrations, buildModels) {
+  public async connect() {
     console.log("🔋 Connecting to database...");
-
-    await runMigrations(pgm);
-
-    const pool = new Pool({
+    await this.migrate();
+    this.pool = new Pool({
       user: PG_USER,
       host: PG_HOST,
       database: PG_DATABASE,
       port: Number(PG_PORT),
     });
+  }
+}
 
-    const models = buildModels(pool);
+export default SpidersDatabase;
 
-    return models;
-  },
-};
+// const SpidersDB: SpidersDatabase = {
+//   models: {},
+//   async migrate() {
+//     console.log("🔧 Running migrations...");
+//     await pgm({
+//       databaseUrl: `postgres://${PG_USER}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`,
+//       migrationsTable: "spiders_migrations",
+//       dir: __dirname + "/migrations",
+//       direction: "up",
+//       count: 9999,
+//     });
+//   },
+//   async model() {
+//     console.log("🔨 Building database models...");
 
-const databaseLayer: DatabaseLayer = { ...DatabaseUtils };
+//     const models: SpidersDatabase.Models = {
+//       users: {
+//         async findUser(login) {
+//           const {
+//             rows: [user],
+//           } = await pool.query(
+//             `SELECT * FROM users WHERE username = '${login.username}'`
+//           );
+//           if (user) return user;
+//         },
+//       },
+//       posts: {
+//         async addPost(post) {
+//           await pool.query(
+//             `INSERT INTO posts (title, author, tags, body, updated_at, user_id, published, published_at)
+//             VALUES (
+//                 '${post.title}',
+//                 '${post.author}',
+//                 '${post.tags}',
+//                 '${post.body}',
+//                 to_timestamp(${Number(post.updatedAt) / 1000}),
+//                 '${post.userId}',
+//                 '${post.published}',
+//                 to_timestamp(${Number(post.publishedAt) / 1000})
+//               )`
+//           );
+//         },
+//       },
+//     };
+//     return models;
+//   },
+//   async connect() {
+//     console.log("🔋 Connecting to database...");
 
-export default databaseLayer;
+//     await SpidersDB.migrate();
+
+//     const pool = new Pool({
+//       user: PG_USER,
+//       host: PG_HOST,
+//       database: PG_DATABASE,
+//       port: Number(PG_PORT),
+//     });
+
+//     const models = await SpidersDB.model(pool);
+
+//     return models;
+//   },
+// };
+
+// export default SpidersDatabase;
