e921f285ab94872aa280325466ddff45b95cbf2e
sidiousvic <sidiousvic@gmail.com>
2020-10-12 22:01:04 +0900

    update navbar heirarchy

--- ui/Spiders/Navbar/Navbar.tsx
+++ ui/Spiders/Navbar/Navbar.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div id="navbar">
-      <div id="spiders">
+    <nav>
+      <div id="title">
         <h1>Spiders 🕸</h1>
         <h2>A web engineering log.</h2>
-        <li>
-          <Link to="/spiders">home</Link>
-        </li>
-        <li>
-          <Link to="/spiders/weave">weave</Link>
-        </li>
       </div>
-    </div>
+      <div id="navlinks">⌰ ⌬ ↯</div>
+    </nav>
   );
 }
