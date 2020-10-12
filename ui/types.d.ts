1d250b84762d2195299d1b44bdf12a2525f85420
sidiousvic <sidiousvic@gmail.com>
2020-10-12 22:01:37 +0900

    declare ts modules

---
+++ ui/types.d.ts
@@ -0,0 +1,6 @@
+declare module "highlight.js/lib/core";
+declare module "highlight.js/lib/languages/javascript";
+declare module "*.css";
+declare module "isomorphic-style-loader/StyleContext";
+declare module "isomorphic-style-loader/withStyles";
+declare module "prismjs";
