bd0c9b64245155fdc80368646481241c644e3d70
sidiousvic <sidiousvic@gmail.com>
2020-10-12 22:03:36 +0900

    add toggleTheme functionality

--- ui/Spiders/Spiders.tsx
+++ ui/Spiders/Spiders.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import routes from "../routes";
 
 export default function Spiders() {
+  const [theme, setTheme] = useState("light");
+  const [lightSwitch, setLightSwitch] = useState("🌔");
+
+  function toggleThemes() {
+    setLightSwitch(lightSwitch === "🌔" ? "🌒" : "🌔");
+    const themes = ["light", "dark"];
+    let currThemeIdx = themes.indexOf(theme);
+    const nextThemeIdx = ++currThemeIdx % themes.length;
+    console.log(themes[nextThemeIdx]);
+    setTheme(themes[nextThemeIdx]);
+  }
+
   return (
-    <>
+    <div id="theme" className={theme}>
+      <span id="light-switch" onClick={toggleThemes}>
+        {lightSwitch}
+      </span>
       <Navbar />
       <Switch>
         {routes.map((route) => (
           <Route key={route.name} {...route} />
         ))}
       </Switch>
-    </>
+    </div>
   );
 }
