cd3cce31ea9f25351e2015ffe0317713d2e4f469
sidiousvic <sidiousvic@gmail.com>
2020-10-12 22:01:24 +0900

    add humanReadableDate func

--- ui/utils.ts
+++ ui/utils.ts
@@ -2,3 +2,28 @@ export function logGraphQLErrors(errors: any) {
   for (let error of errors)
     console.error(`GraphQL response error: ${error.message}`);
 }
+
+export function getHumanReadableDate() {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const days = ["日", "月", "火", "水", "木", "金", "土"];
+  const date = new Date();
+  const month = months[date.getMonth()];
+  const day = days[date.getDay()];
+  const number = date.getDate();
+  const year = date.getFullYear();
+  const humanReadableDate = `${day}, ${number} ${month} ${year}`;
+  return humanReadableDate;
+}
