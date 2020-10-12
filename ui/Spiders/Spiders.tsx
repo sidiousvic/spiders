07cee0218fe84862f080877649f04bf2ba5f839f
sidiousvic <sidiousvic@gmail.com>
2020-10-12 23:49:14 +0900

    implement cookie theme recall/switch

--- ui/Spiders/Spiders.tsx
+++ ui/Spiders/Spiders.tsx
@@ -1,32 +1,44 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import routes from "../routes";
+import Cookie from "cookie-universal";
+const cookies = Cookie();
 
 export default function Spiders() {
-  const [theme, setTheme] = useState("light");
-  const [lightSwitch, setLightSwitch] = useState("🌔");
+  const cookieTheme = cookies.get("theme");
+  const [theme, setTheme] = useState(cookieTheme);
+  const [lightSwitch, setLightSwitch] = useState(
+    theme === "dark" ? "🌒" : "🌔"
+  );
+
+  useLayoutEffect(() => {
+    cookies.set("theme", theme, {
+      path: "/spiders",
+    });
+  }, [theme]);
 
   function toggleThemes() {
-    setLightSwitch(lightSwitch === "🌔" ? "🌒" : "🌔");
+    setLightSwitch(lightSwitch === "🌒" ? "🌔" : "🌒");
     const themes = ["light", "dark"];
     let currThemeIdx = themes.indexOf(theme);
     const nextThemeIdx = ++currThemeIdx % themes.length;
-    console.log(themes[nextThemeIdx]);
     setTheme(themes[nextThemeIdx]);
   }
 
   return (
-    <div id="theme" className={theme}>
-      <span id="light-switch" onClick={toggleThemes}>
-        {lightSwitch}
-      </span>
-      <Navbar />
-      <Switch>
-        {routes.map((route) => (
-          <Route key={route.name} {...route} />
-        ))}
-      </Switch>
-    </div>
+    theme && (
+      <div id="theme" className={theme}>
+        <span id="light-switch" onClick={toggleThemes}>
+          {lightSwitch}
+        </span>
+        <Navbar />
+        <Switch>
+          {routes.map((route) => (
+            <Route key={route.name} {...route} />
+          ))}
+        </Switch>
+      </div>
+    )
   );
 }
