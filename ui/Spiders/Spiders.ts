cd1fc6b2012cbe07dd7bbdadc806468b92e4aadb
sidiousvic <sidiousvic@gmail.com>
2020-10-13 22:38:37 +0900

    normalize Spiders component

---
+++ ui/Spiders/Spiders.ts
@@ -0,0 +1,84 @@
+import { getTimeOfDayTheme, getHumanReadableDate } from "../utils";
+
+export default function Spiders() {
+  const timeOfDayTheme = getTimeOfDayTheme();
+  const theme = timeOfDayTheme;
+  const lightSwitch = theme === "dark" ? "🌒" : "🌔";
+
+  // function toggleThemes() {
+  //   setLightSwitch(lightSwitch === "🌒" ? "🌔" : "🌒");
+  //   const themes = ["light", "dark"];
+  //   let currThemeIdx = themes.indexOf(theme);
+  //   const nextThemeIdx = ++currThemeIdx % themes.length;
+  //   setTheme(themes[nextThemeIdx]);
+  // }
+
+  return `<div id="theme" class=${theme}>
+        <span id="light-switch">
+          ${lightSwitch}
+        </span>
+        <nav>
+      <div id="title">
+        <h1>Spiders 🕸</h1>
+        <h2>A web engineering log.</h2>
+      </div>
+      <div id="navlinks">⌰ ⌬ ↯</div>
+    </nav>
+        <div id="content">
+      <div id="posts">
+        <div class="post">
+          <h1 class="post-title">Launching your Apollo Server</h1>
+          <h2 class="post-subtitle">by Vic @ ${getHumanReadableDate()}</h2>
+          <div class="post-body">
+            <p>
+              Sometimes it's useful to initialize processes in your pipeline by
+              wrapping them in a function. In here, we're connecting to a
+              database, and then initializing a server with Apollo. We also
+              compute the context for resolvers depending on whether the user is
+              authenticated or not; that logic, however, is abstracted somewhere
+              else. I enjoy the declarative style of calling a{" "}
+              <code>launchSpidersServer</code> function in the end, akin to
+              turning the key in your car.
+            </p>
+            <pre class="language-typescript">
+              <code>
+                ${`launchSSRServer();
+
+async function launchSpidersServer(
+  database: SpidersDatabase,
+  { typeDefs, resolvers, auth, utils: { computeContext } }: GraphQL.Layer) {
+  
+  await database.connect();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers: resolvers,
+    async context({ req }) {
+      return await computeContext(req, database, auth);
+    },
+  });
+
+  server.listen({ port: 9991 }).then(() => {
+    console.log(\`🚀 Apollo Server launched @ \${graphqlServerUri}\`);
+  });
+}
+
+launchSpidersServer(new SpidersDatabase(), graphqlLayer);`}
+              </code>
+            </pre>
+            <p>
+              I think of my internal services as uniform APIs. With Typescript,
+              the tools to design interfaces that communicate seamlessly with
+              each other are there. For each layer of my stack—from the
+              database, to the middle end GraphQL (including authentication
+              middleware) we create API layers which expose methods and helpers
+              related in funcionality with one another.
+            </p>
+          </div>
+          <br />
+          <sub class="tags">#pancakes #yeahboy</sub>
+        </div>
+      </div>
+    </div>
+      </div>`;
+}
---
+++ ui/index.ts
@@ -0,0 +1,14 @@
+import Spiders from "./Spiders/Spiders";
+import "./styles.css";
+import Prism from "prismjs";
+import "./spiders.code.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/line-highlight/prism-line-highlight.css";
+import "prismjs/components/prism-typescript";
+const env = process.env.NODE_ENV;
+
+Prism.highlightAll();
+
+const SpidersUI = Spiders();
+
+document.body.innerHTML = SpidersUI;
