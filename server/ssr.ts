1ac0fba54fc47eac36620f8ec394af75e6563c85
sidiousvic <sidiousvic@gmail.com>
2020-10-14 21:12:39 +0900

    tsx > ts

---
+++ server/ssr.ts
@@ -0,0 +1,41 @@
+import Spiders from "../ui/Spiders/Spiders";
+import express from "express";
+
+// const env = process.env.NODE_ENV;
+
+const SpidersUI = Spiders();
+
+function launchSSRServer() {
+  const app = express();
+  app.get("/spiders", async (_, res) => {
+    const serverRenderedHTML = html();
+    res.send(`<!DOCTYPE html>${serverRenderedHTML}`);
+  });
+
+  app.listen(9992, () => {
+    console.log(`🧪 SSR Spiders is now running @ 9992!`);
+  });
+
+  function html() {
+    return `<html lang="en">
+          <head>
+            <title>Spiders, a Web Engineering Log.</title>
+            <meta
+              name="description"
+              content="A weblog for web engineers. Web weaving (programming), www infrastructure, application architecture, and design with your healthy dose of aliens, pizza and punk rock."
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="styles.css" />
+          </head>
+          <body>
+            <div id="spiders">
+              ${SpidersUI}
+            </div>
+            <script defer src="ui.js"></script>
+          </body>
+        </html>`;
+  }
+}
+
+export default launchSSRServer;
