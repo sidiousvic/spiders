cd8789db9bdd51d1fa6236cef3fb56868c829a21
sidiousvic <sidiousvic@gmail.com>
2020-10-15 20:12:26 +0900

    run Spiders

--- ui/index.ts
+++ ui/index.ts
@@ -1,13 +1,1 @@
-import Spiders from "./Spiders/Spiders";
-import "./styles.css";
-import Prism from "prismjs";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/line-highlight/prism-line-highlight.css";
-import "prismjs/components/prism-typescript";
-const env = process.env.NODE_ENV;
-
-Prism.highlightAll();
-
-const SpidersUI = Spiders();
-
-document.body.innerHTML = SpidersUI;
+import "./Spiders/Spiders";
