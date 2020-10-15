6f1b3a1736698dad49e67a29391964892608abf7
sidiousvic <sidiousvic@gmail.com>
2020-10-15 20:12:20 +0900

    using LitElement

--- ui/Spiders/Spiders.ts
+++ ui/Spiders/Spiders.ts
@@ -1,89 +1,105 @@
-import { getTimeOfDayTheme, getHumanReadableDate, isBrowser } from "../utils";
+import Prism from "prismjs";
+import "prismjs/components/prism-typescript";
+import { spidersStyles } from "./../spiderStyles";
+import { spidersCodeStyles } from "./../spidersCodeStyles";
+import { getTimeOfDayTheme, getHumanReadableDate } from "../utils";
+import { LitElement as X, html, property, customElement } from "lit-element";
 
-export default function Spiders() {
-  const timeOfDayTheme = getTimeOfDayTheme();
-  let theme = timeOfDayTheme;
-  let lightSwitch = theme === "dark" ? "🌒" : "🌔";
+@customElement("x-spiders")
+export default class Spiders extends X {
+  @property() name = "World";
+  @property() timeOfDayTheme = getTimeOfDayTheme();
+  @property() theme = this.timeOfDayTheme;
+  @property() lightSwitch = this.theme === "dark" ? "🌒" : "🌔";
 
-  function toggleThemes() {
-    lightSwitch = lightSwitch === "🌒" ? "🌔" : "🌒";
+  toggleThemes() {
+    this.lightSwitch = this.lightSwitch === "🌒" ? "🌔" : "🌒";
     const themes = ["light", "dark"];
-    let currThemeIdx = themes.indexOf(theme);
+    let currThemeIdx = themes.indexOf(this.theme);
     const nextThemeIdx = ++currThemeIdx % themes.length;
-    theme = themes[nextThemeIdx];
+    this.theme = themes[nextThemeIdx];
   }
 
-  isBrowser &&
-    document.addEventListener("click", (e) => {
-      if ((e.target as HTMLElement).id === "light-switch") toggleThemes();
-    });
+  static styles = [spidersStyles, spidersCodeStyles];
 
-  return `<div id="theme" class=${theme}>
-        <span id="light-switch">
-          ${lightSwitch}
-        </span>
-        <nav>
-      <div id="title">
-        <h1>Spiders 🕸</h1>
-        <h2>A web engineering log.</h2>
-      </div>
-      <div id="navlinks">⌰ ⌬ ↯</div>
-    </nav>
-        <div id="content">
-      <div id="posts">
-        <div class="post">
-          <h1 class="post-title">Launching your Apollo Server</h1>
-          <h2 class="post-subtitle">by Vic @ ${getHumanReadableDate()}</h2>
-          <div class="post-body">
-            <p>
-              Sometimes it's useful to initialize processes in your pipeline by
-              wrapping them in a function. In here, we're connecting to a
-              database, and then initializing a server with Apollo. We also
-              compute the context for resolvers depending on whether the user is
-              authenticated or not; that logic, however, is abstracted somewhere
-              else. I enjoy the declarative style of calling a{" "}
-              <code>launchSpidersServer</code> function in the end, akin to
-              turning the key in your car.
-            </p>
-            <pre class="language-typescript">
-              <code>
-                ${`launchSSRServer();
+  firstUpdated() {
+    const code = this.shadowRoot?.querySelector("#code");
+    (code as HTMLElement).innerHTML = Prism.highlight(
+      (code as HTMLElement).innerText,
+      Prism.languages.typescript,
+      "typescript"
+    );
+  }
+
+  render() {
+    return html` <div id="theme" class=${this.theme}>
+      <span id="light-switch"> ${this.lightSwitch} </span>
+      <nav>
+        <div id="title">
+          <h1>Spiders 🕸</h1>
+          <h2>A web engineering log.</h2>
+        </div>
+        <div id="navlinks">⌰ ⌬ ↯</div>
+      </nav>
+      <div id="content">
+        <div id="posts">
+          <div class="post">
+            <h1 class="post-title">Launching your Apollo Server</h1>
+            <h2 class="post-subtitle">by Vic @ ${getHumanReadableDate()}</h2>
+            <div class="post-body">
+              <p>
+                Sometimes it's useful to initialize processes in your pipeline
+                by wrapping them in a function. In here, we're connecting to a
+                database, and then initializing a server with Apollo. We also
+                compute the context for resolvers depending on whether the user
+                is authenticated or not; that logic, however, is abstracted
+                somewhere else. I enjoy the declarative style of calling a{" "}
+                <code>launchSpidersServer</code> function in the end, akin to
+                turning the key in your car.
+              </p>
+              <pre class="language-typescript">
+          <code id="code">
+${`
+
+launchSSRServer();
 
 async function launchSpidersServer(
-  database: SpidersDatabase,
-  { typeDefs, resolvers, auth, utils: { computeContext } }: GraphQL.Layer) {
-  
-  await database.connect();
+database: SpidersDatabase,
+{ typeDefs, resolvers, auth, utils: { computeContext } }: GraphQL.Layer) {
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers: resolvers,
-    async context({ req }) {
-      return await computeContext(req, database, auth);
-    },
-  });
+await database.connect();
 
-  server.listen({ port: 9991 }).then(() => {
-    console.log(\`🚀 Apollo Server launched @ \${graphqlServerUri}\`);
-  });
+const server = new ApolloServer({
+typeDefs,
+resolvers: resolvers,
+async context({ req }) {
+  return await computeContext(req, database, auth);
+},
+});
+
+server.listen({ port: 9991 }).then(() => {
+console.log(\`🚀 Apollo Server launched @ \${graphqlServerUri}\`);
+});
 }
 
-launchSpidersServer(new SpidersDatabase(), graphqlLayer);`}
-              </code>
-            </pre>
-            <p>
-              I think of my internal services as uniform APIs. With Typescript,
-              the tools to design interfaces that communicate seamlessly with
-              each other are there. For each layer of my stack—from the
-              database, to the middle end GraphQL (including authentication
-              middleware) we create API layers which expose methods and helpers
-              related in funcionality with one another.
-            </p>
+launchSpidersServer(new SpidersDatabase(), graphqlLayer);
+`}
+          </code>
+        </pre>
+              <p>
+                I think of my internal services as uniform APIs. With
+                Typescript, the tools to design interfaces that communicate
+                seamlessly with each other are there. For each layer of my
+                stack—from the database, to the middle end GraphQL (including
+                authentication middleware) we create API layers which expose
+                methods and helpers related in funcionality with one another.
+              </p>
+            </div>
+            <br />
+            <sub class="tags">#pancakes #yeahboy</sub>
           </div>
-          <br />
-          <sub class="tags">#pancakes #yeahboy</sub>
         </div>
       </div>
-    </div>
-      </div>`;
+    </div>`;
+  }
 }
