674e9943fb79767cff7d154756e2711bdd069731
sidiousvic <sidiousvic@gmail.com>
2020-10-23 15:22:21 +0900

    remove inline typings

--- server/gql/utils.ts
+++ server/gql/utils.ts
@@ -1,15 +1,14 @@
-import { SpidersDatabase } from "../db";
-import { Utils, Context, User } from "../../types";
+import { Utils, User } from "@spiders";
 import { auth } from "./auth";
 
 const { getUserIdFromToken } = auth;
 
 const utils: Utils = {
-  async computeContext(req: any, database: SpidersDatabase): Promise<Context> {
+  async computeContext(req, database) {
     const token = req.headers.authorization;
     let authedUser = {} as User;
     if (token) {
-      const id = getUserIdFromToken(token as string);
+      const id = getUserIdFromToken(token);
       authedUser = await database.findUser({ id });
     }
     return { database, authedUser };
