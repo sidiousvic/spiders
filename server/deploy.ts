e73e1921428533959391b9552a92c0a94ccb5955
sidiousvic <sidiousvic@gmail.com>
2020-10-28 21:15:17 +0900

    sp

--- server/deploy.ts
+++ server/deploy.ts
@@ -40,7 +40,7 @@ async function launchDeployServer() {
   });
 
   async function deploy() {
-    console.log(`⛓ Running deploy script...`);
+    console.log(`⛓  Running deploy script...`);
     const { stdout, stderr } = await exec("/var/www/spiders/deploy.sh");
     if (stderr) console.error(`Error during deploy script: ${stderr}`);
     console.log(stdout);
