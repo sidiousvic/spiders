7a909cb1fb9a4d017457231c81b15d550dc0c96f
sidiousvic <sidiousvic@gmail.com>
2020-11-16 21:48:32 +0900

    weaverMachine added states

--- ui/machines/weaverMachine.ts
+++ ui/machines/weaverMachine.ts
@@ -1,21 +1,25 @@
 import { Machine, interpret } from "xstate";
 
+export type WeaverEvents = "TOGGLE_MODE" | "STAGE" | "COMMIT";
+
 export interface WeaverStateSchema {
   states: {
     weave: {};
     read: {};
+    staged: {};
   };
 }
 
-export type WeaverToggleEvent = { type: "TOGGLE" };
-export type Modes = "weave" | "read";
+export type WeaverStateEvent = { type: WeaverEvents };
+export type Modes = "weave" | "read" | "staged";
 
-export const weaverMachine = Machine<{}, WeaverStateSchema, WeaverToggleEvent>({
+export const weaverMachine = Machine<{}, WeaverStateSchema, WeaverStateEvent>({
   id: "weaverMachine",
   initial: "weave",
   states: {
-    weave: { on: { TOGGLE: "read" } },
-    read: { on: { TOGGLE: "weave" } },
+    weave: { on: { TOGGLE_MODE: "read", STAGE: "staged" } },
+    read: { on: { TOGGLE_MODE: "weave", STAGE: "staged" } },
+    staged: { on: { COMMIT: "weave", TOGGLE_MODE: "weave" } },
   },
 });
 
