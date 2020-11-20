5e457afefd9b2c4a3782c865d41239746b5139eb
sidiousvic <sidiousvic@gmail.com>
2020-11-20 23:52:09 +0900

    expand weaver state error modes

--- ui/machines/weaverMachine.ts
+++ ui/machines/weaverMachine.ts
@@ -1,26 +1,82 @@
-import { Machine, interpret } from "xstate";
+import { Post } from "spiders";
+import { Machine, interpret, assign } from "xstate";
 
-export type WeaverEvents = "TOGGLE_MODE" | "STAGE" | "COMMIT";
+const assignPost = assign({
+  post: (ctx, e) => (e as WeaverStateEvent).post,
+});
+
+export type WeaverEvents =
+  | "TOGGLE_MODE"
+  | "UPDATE"
+  | "STAGE"
+  | "POSTED"
+  | "POST_ERROR"
+  | "EMPTY_TITLE_ERROR"
+  | "EMPTY_BODY_ERROR"
+  | "EMPTY_TAGS_ERROR"
+  | "RESET";
 
 export interface WeaverStateSchema {
   states: {
     weave: {};
     read: {};
     staged: {};
+    posted: {};
+    postError: {};
+    emptyTitleError: {};
+    emptyBodyError: {};
+    emptyTagsError: {};
   };
 }
 
-export type WeaverStateEvent = { type: WeaverEvents };
-export type Modes = "weave" | "read" | "staged";
+export type WeaverStateEvent = { type: WeaverEvents; post: Partial<Post> };
 
-export const weaverMachine = Machine<{}, WeaverStateSchema, WeaverStateEvent>({
-  id: "weaverMachine",
-  initial: "weave",
-  states: {
-    weave: { on: { TOGGLE_MODE: "read", STAGE: "staged" } },
-    read: { on: { TOGGLE_MODE: "weave", STAGE: "staged" } },
-    staged: { on: { COMMIT: "weave", TOGGLE_MODE: "weave" } },
+export const weaverMachine = Machine<
+  { post: Partial<Post> },
+  WeaverStateSchema,
+  WeaverStateEvent
+>(
+  {
+    id: "weaverMachine",
+    context: {
+      post: {},
+    },
+    initial: "weave",
+    states: {
+      weave: {
+        on: {
+          TOGGLE_MODE: "read",
+          STAGE: "staged",
+          RESET: { target: "weave", actions: [assignPost] },
+          UPDATE: { target: "weave", actions: [assignPost] },
+        },
+      },
+      read: {
+        on: {
+          TOGGLE_MODE: "weave",
+          STAGE: "staged",
+          RESET: { target: "weave", actions: [assignPost] },
+          UPDATE: { target: "weave", actions: [assignPost] },
+        },
+      },
+      staged: {
+        on: {
+          POSTED: "posted",
+          TOGGLE_MODE: "weave",
+          EMPTY_TITLE_ERROR: "emptyTitleError",
+          EMPTY_BODY_ERROR: "emptyBodyError",
+        },
+      },
+      posted: { on: { RESET: "weave" } },
+      postError: { on: { RESET: "weave" } },
+      emptyTitleError: { on: { RESET: "weave" } },
+      emptyBodyError: { on: { RESET: "weave" } },
+      emptyTagsError: { on: { RESET: "weave" } },
+    },
   },
-});
+  {
+    actions: { assignPost },
+  }
+);
 
 export const weaverService = interpret(weaverMachine);
