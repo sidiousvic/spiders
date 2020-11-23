ad137ad2d5af458a274c2a81705131229b68ef09
sidiousvic <sidiousvic@gmail.com>
2020-11-23 20:30:54 +0900

    copy upd

--- ui/components/XPostCard.ts
+++ ui/components/XPostCard.ts
@@ -117,7 +117,7 @@ export default class XPostCard extends X {
                   : ""}
                 @click=${this.cancelDeletePost}
               >
-                OH SHIT NONONO
+                OH SHIT NO
               </div>`
             : ""} `;
       }
