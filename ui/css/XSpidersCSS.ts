07a2c61cd09835550384724283a7daa24dd41e36
sidiousvic <sidiousvic@gmail.com>
2020-11-25 15:54:03 +0900

    styles updates

--- ui/css/XSpidersCSS.ts
+++ ui/css/XSpidersCSS.ts
@@ -14,49 +14,80 @@ const XSpidersCSS = css`
     --spiders-blue: #195aff;
     --spiders-blue-dark: rgb(20, 0, 222);
     --spiders-blue-light: rgb(40, 20, 222);
-    --spiders-zef: rgb(225, 225, 235);
-    --spiders-zef-dark: rgb(215, 215, 225);
-    --spiders-zef-light: rgb(230, 230, 250);
+    --spiders-bone-light: rgb(240, 240, 240);
     --spiders-bone: rgb(230, 230, 230);
     --spiders-bone-dark: rgb(220, 220, 220);
     --spiders-gray: rgb(8, 8, 8);
-    --spiders-gray-light: rgb(12, 12, 12);
+    --spiders-gray-light: rgb(15, 15, 15);
     --spiders-gray-dark: rgb(4, 4, 4);
   }
 
   .dark {
     --background: var(--spiders-gray);
+    --background-2: var(--spiders-gray-light);
+    --background-pattern: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAAR0lEQVQoU43Q0QkAIAgE0HON23821zAKjLQL8k99omUAAh9hCpKEu5fxCybquMDePPMN1bq5O+sLvlAeOftGMvrh6hPkqxUcp58jT1ArFZIAAAAASUVORK5CYII=)
+      repeat;
     --foreground: var(--spiders-bone);
-    --code-background: var(--spiders-gray-light);
-    --code-foreground: var(--spiders-bone-dark);
-    --background-light: var(--spiders-gray-light);
+    --foreground-2: var(--spiders-bone-dark);
     --accent: var(--spiders-green);
-    --accent-gradient: var(--spiders-mint);
+    --accent-2: var(--spiders-mint);
+    --shadows: var(--spiders-gray-dark);
+    --floodlights: var(--spiders-mint);
+    --theme-borders: none;
+    --gradient: -webkit-linear-gradient(
+      90deg,
+      var(--background-2),
+      var(--accent),
+      var(--accent-2)
+    );
+    --gradient-2: -webkit-linear-gradient(
+      90deg,
+      var(--background-2),
+      var(--accent),
+      var(--accent-2)
+    );
+    --gradient-2: -webkit-linear-gradient(90deg, navy, cyan, blue);
+    --gradient-3: -webkit-linear-gradient(90deg, red, lightpink, red);
+    --dark-foreground: var(--foreground);
   }
 
   .light {
-    --background: lavenderblush;
-    --foreground: var(--spiders-pastel-dark);
-    --code-background: floralwhite;
-    --code-foreground: var(--spiders-peach);
-    --background-light: floralwhite;
-    --accent: var(--spiders-pastel);
-    --accent-gradient: var(--spiders-pastel-light);
+    --background: ghostwhite;
+    --background-2: ghostwhite;
+    --background-pattern: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAICAYAAADA+m62AAAATElEQVQoU2P88eP/fwYkwH50H8NPaydkITCbEVkhTBE2xXCF6JLofLBCXNYhizP+37v3PzY3wRwJU4ziRgwfQAVAiolSiOFrXCaCxAGDUzoDcAdK2gAAAABJRU5ErkJggg==)
+      repeat;
+    --foreground: var(--spiders-gray-light);
+    --foreground-2: var(--spiders-gray-light);
+    --accent: orangered;
+    --accent-2: red;
+    --shadows: orangered;
+    --floodlights: var(--spiders-peach);
+    --theme-borders: solid 1px var(--accent-2);
+    --gradient: -webkit-linear-gradient(
+      90deg,
+      var(--spiders-peach),
+      var(--accent),
+      var(--accent-2)
+    );
+    --gradient-2: -webkit-linear-gradient(90deg, navy, cyan, blue);
+    --gradient-3: -webkit-linear-gradient(90deg, red, lightpink, red);
+    --dark-foreground: var(--background);
   }
 
   #spiders {
-    font-family: Dank Mono, sans-serif;
+    font-family: Wired Mono, sans-serif;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-    border-radius: 2px;
-    background: var(--background);
+    background: var(--background-pattern);
     color: var(--foreground);
     transition: ease-in-out 0.3s;
-    height: fit-content;
-    min-height: 100vh;
+    height: 95vh;
     overflow: none;
+    position: relative;
+    overflow: scroll;
+    scroll-padding-bottom: 15vh;
   }
 
   *::selection {
@@ -66,6 +97,26 @@ const XSpidersCSS = css`
   *::-moz-selection {
     background: var(--spiders-mint-light);
   }
+
+  #spiders:nth-child(2) {
+    /* height: 30vh; */
+  }
+
+  #footer {
+    border-top: 1px var(--accent-2) solid;
+    height: 5vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--foreground);
+    background: var(--background);
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    text-align: center;
+    transition: ease-in-out 200ms;
+  }
+
   .floodlights {
     box-shadow: 0px -20px 200px var(--floodlights);
   }
