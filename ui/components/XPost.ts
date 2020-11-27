b01695e8cdd8d6c4850a231927216ebb8a70c7eb
sidiousvic <sidiousvic@gmail.com>
2020-11-27 13:18:51 +0900

    create template Post component

--- ui/components/XPost.ts
+++ ui/components/XPost.ts
@@ -1,85 +1,38 @@
-import { UserAuth } from "spiders";
+import { Post, UserAuth } from "spiders";
 import { LitElement as X, html, property, customElement } from "lit-element";
-import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
-import Prism from "prismjs";
+import "./XPostCard";
+import truncateHTML from "truncate-html";
+import { unsafeHTML } from "lit-html/directives/unsafe-html";
 import { XPostCSS } from "../css/XPostCSS";
-import { spidersCodeCSS } from "../css/SpidersCodeCSS";
-import {
-  event,
-  fireGraphQLQuery,
-  getHumanReadableDate,
-  logGraphQLErrors,
-} from "../utils";
-import "prismjs/components/prism-typescript";
+import { getHumanReadableDate } from "../utils";
 
-@customElement("x-post")
-export default class XPost extends X {
+@customElement("x-post-cards")
+export default class XPostCards extends X {
   @property() theme = "";
+  @property() loadingMessage = "Weaving webs...";
   @property() auth: UserAuth;
-  @property({ type: Object }) post: any;
+  @property() post: Partial<Post> = {};
 
-  static styles = [XPostCSS, spidersCodeCSS];
+  connectedCallback() {}
 
-  firstUpdated() {
-    Prism.highlightAllUnder(this.shadowRoot);
-  }
-
-  async handleDeletePost() {
-    const deletePostMutation = `
-    mutation deletePost($input: DeletePostInput!) {
-      deletePost(input: $input) {
-        message
-        resource
-      }
-    }
-    `;
-    const variables = {
-      input: {
-        postId: this.post.postId,
-      },
-    };
-
-    const headers = {
-      authorization: this.auth.token,
-    };
-
-    const { errors } = await fireGraphQLQuery(
-      deletePostMutation,
-      variables,
-      headers
-    );
-
-    if (errors) {
-      logGraphQLErrors(errors);
-    }
-
-    const onPostDelete = event("onPostDelete", { postId: this.post.postId });
-    this.dispatchEvent(onPostDelete);
-  }
-
-  renderDeletePostButton() {
-    switch (this.auth.user.role) {
-      case "DARKLORD": {
-        return html`<icon id="deletePostButton" @click=${this.handleDeletePost}
-          >🔥‍</icon
-        >`;
-      }
-      default:
-        return html``;
-    }
-  }
+  static styles = [XPostCSS];
 
   render() {
+    const postBodyVisual = unsafeHTML(this.post.body.split("</pre>")[0]);
+    const postBodyText = unsafeHTML(
+      truncateHTML(this.post.body.split("</pre>")[1] || "NO VISUAL", 12, {
+        byWords: true,
+        excludes: ["code"],
+      })
+    );
     return html`
       <div class="post ${this.theme}">
         <h1 class="post-title">${this.post.title}</h1>
         <h2 class="post-subtitle">
-          by ${this.post.author} @
           ${getHumanReadableDate(new Date(this.post.createdAt))}
         </h2>
-        <div class="post-body">${unsafeHTML(this.post.body)}</div>
-        <sub class="tags">${this.post.tags}</sub>
-        ${this.renderDeletePostButton()}
+        <div class="post-card-body">${postBodyVisual}${postBodyText}</div>
+        <sub class="post-card-tags">${this.post.tags}</sub>
       </div>
     `;
   }
