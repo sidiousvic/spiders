589e918d930aa10d70ed86e7e5c8af2a9eb57b9d
sidiousvic <sidiousvic@gmail.com>
2021-01-10 21:44:18 +0900

    data machine working

--- ui/components/XSpiders.ts
+++ ui/components/XSpiders.ts
@@ -1,10 +1,18 @@
 import { LitElement as X, html, customElement } from "lit-element";
 import { XSpidersCSS } from "../css/XSpidersCSS";
 import { UniversalCSS } from "../css/UniversalCSS";
+import { dataMachine } from "../machines/dataMachine";
 
 @customElement("x-spiders")
 class XSpiders extends X {
   static styles = [UniversalCSS, XSpidersCSS];
+
+  firstUpdated() {
+    dataMachine.send("FETCH_POSTS");
+    dataMachine.onTransition((state) => {
+      console.log(state);
+    });
+  }
 
   render() {
     return html`Spiders`;
---
+++ ui/machines/dataMachine.ts
@@ -0,0 +1,85 @@
+import { Machine, interpret, assign } from "xstate";
+import { logGraphQLErrors } from "../utils";
+
+const dataBlueprint = Machine(
+  {
+    initial: "idle",
+    context: {
+      posts: {},
+      message: "",
+    },
+    on: {
+      FETCH_POSTS: "fetchingPosts",
+    },
+    states: {
+      idle: {},
+      fetchingPosts: {
+        invoke: [
+          {
+            src: "fetchPosts",
+            onDone: {
+              target: "postsFetched",
+              actions: ["onSuccess"],
+            },
+            onError: {
+              target: "fetchError",
+              actions: ["onError"],
+            },
+          },
+        ],
+      },
+      postsFetched: { entry: [(ctx) => console.log(ctx)] },
+      fetchError: {},
+    },
+  },
+  {
+    services: {
+      async fetchPosts() {
+        const res = await fetch("/graphql", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            query: `{ 
+                  findPosts { 
+                    postId
+                    title 
+                    author 
+                    body 
+                    raw
+                    createdAt 
+                    tags
+                  }
+                }`,
+          }),
+        });
+
+        const {
+          data: { findPosts },
+        } = await res.json();
+
+        return { posts: { ...findPosts } };
+      },
+    },
+    actions: {
+      onSuccess: assign((X, { data }: any) => {
+        return { ...data };
+      }),
+      onError: assign((X, { data }: any) => {
+        const message = data;
+        console.error(message);
+        return { message };
+      }),
+      onWarning: assign((X, { data }: any) => {
+        const message = data;
+        console.warn(message);
+        return { message };
+      }),
+    },
+  }
+);
+
+const dataMachine = interpret(dataBlueprint).start();
+
+export { dataMachine };
